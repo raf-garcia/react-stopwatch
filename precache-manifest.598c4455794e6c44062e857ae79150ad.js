929d9d0a1560083614bf035bf8f8602ed4c3db2e
Rafael.Garcia <rafael.garcia@RafaelGarcias-MacBook-Pro.local>
2019-06-06 15:58:32 -0400

    Updates

---
+++ precache-manifest.598c4455794e6c44062e857ae79150ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "666a19224cd9a047bdf6e1a9ff1d38a0",
+    "url": "/react-stopwatch/index.html"
+  },
+  {
+    "revision": "4e4ca667d122b7f67158",
+    "url": "/react-stopwatch/static/css/main.e6eb9d70.chunk.css"
+  },
+  {
+    "revision": "609c56dfd95f9cf6673d",
+    "url": "/react-stopwatch/static/js/2.c1ec755e.chunk.js"
+  },
+  {
+    "revision": "4e4ca667d122b7f67158",
+    "url": "/react-stopwatch/static/js/main.5da1706f.chunk.js"
+  },
+  {
+    "revision": "1e6f92fa7bd110bc0e8e",
+    "url": "/react-stopwatch/static/js/runtime~main.b7a26dfe.js"
+  }
+]);
